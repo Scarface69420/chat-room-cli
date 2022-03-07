e5e2215af4604b28aefcad899d922ea1089ff1c2
Scarface69420 <nixonkurian.nk@gmail.com>
2022-03-08 00:53:35 +0530

    First commit

---
+++ client.py
@@ -0,0 +1,31 @@
+from email import message
+from http import client
+import socket,threading
+
+nickname = input("Choose your nickname: ")
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(('127.0.0.1',6969))
+
+def receive():
+    while True:
+        try:
+            message = client.recv(1024).decode('ascii')
+            if message == 'NICKNAME':
+                client.send(nickname.encode('ascii'))
+            else:
+                print(message)
+        except:
+            print("An error occured!")
+            client.close()
+            break
+
+def write():
+    while True:
+        message = "{}: {}".format(nickname, input(''))
+        client.send(message.encode('ascii'))
+
+receive_thread = threading.Thread(target=receive)
+receive_thread.start()
+write_thread = threading.Thread(target=write)
+write_thread.start()
